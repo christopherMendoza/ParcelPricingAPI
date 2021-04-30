c4868f34850b91dcd9416821574fe187222dff34
Cristopher Mendoza <mendoza.chris39@gmail.com>
2021-04-30 23:18:58 +0800

    Added service implementation layer

--- src/main/java/com/mynt/parcelpricing/service/ParcelPricingService.java
+++ src/main/java/com/mynt/parcelpricing/service/ParcelPricingService.java
@@ -1,23 +1,11 @@
 package com.mynt.parcelpricing.service;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.mynt.parcelpricing.bean.ParcelBean;
 import com.mynt.parcelpricing.bean.PaymentBean;
-import com.mynt.parcelpricing.constant.AppConstant;
 import com.mynt.parcelpricing.exception.ApiRequestException;
 import com.mynt.parcelpricing.model.MathEquation;
-import com.mynt.parcelpricing.model.VoucherItem;
-import com.mynt.parcelpricing.util.FileUtil;
-import com.mynt.parcelpricing.util.FormulaEvaluator;
-
-import io.netty.util.internal.StringUtil;
 
 /**
  * Service class that handles the main logic of Parcel pricing computation.
@@ -26,79 +14,9 @@ import io.netty.util.internal.StringUtil;
  * @since 4/27/2021
  *
  */
-@Service
-public class ParcelPricingService {
+public interface ParcelPricingService {
 
-	@Autowired
-	private FormulaEvaluator formulaEvaluator;
-	
-	@Autowired
-	private VoucherService voucherService;
-	
-	public PaymentBean compute(ParcelBean bean) throws ApiRequestException {
-		
-		PaymentBean paymentBean = new PaymentBean();
-		bean.computeVolume();
-		
-		List<MathEquation> equationList = FileUtil.loadMathEquation();
-		for(MathEquation equation: equationList) {
-			BigDecimal resultCondition = formulaEvaluator.evaluate(equation.getCondition(),bean);
-			
-			/**
-			 * If result condition is 0 means condition did not meet
-			 * then iterate with the next condition.
-			 */
-			if(resultCondition.compareTo(BigDecimal.ZERO) < 1) {
-				continue;
-			}
-			
-			/**
-			 * Condition met but the equation is not application or rejected.
-			 */
-			if(equation.getEquation().equals(AppConstant.NA)) {
-				throw new ApiRequestException("Transaction is rejected weight is more than the allowable limit");
-			}
-			
-			/**
-			 * Compute for the pricing base on the equation set.
-			 */
-			BigDecimal shippingFee = formulaEvaluator.evaluate(equation.getEquation(),bean).setScale(2,RoundingMode.HALF_UP);
-			paymentBean.setShippingFee(shippingFee);
-			paymentBean.setTotal(shippingFee);
-			
-			//Check if there is voucher code pass from the request.
-			this.validateAndProcessVoucher(bean, paymentBean);
-			
-			break;
-		}
-		
-		return paymentBean;
-	}
+	public PaymentBean compute(ParcelBean bean) throws ApiRequestException;
 
-	/**
-	 * Validated voucher code and get the details.
-	 * 
-	 * @param bean - the request bean object.
-	 * @param paymentBean - the payment bean object.
-	 */
-	private void validateAndProcessVoucher(ParcelBean bean, PaymentBean paymentBean) throws ApiRequestException {
-		String voucherCode = bean.getVoucherCode();
-		if(!StringUtil.isNullOrEmpty(voucherCode)) {
-			VoucherItem voucherItem = voucherService.getVoucher(voucherCode);
-			
-			if(voucherItem == null) {
-				throw new ApiRequestException("Voucher code " + voucherCode + "is invalid");			}
-			
-			if(voucherItem != null) {
-				paymentBean.setDiscount(voucherItem.getDiscount());
-			}
-		}
-	}
-	
-	public List<MathEquation> equationlist() throws ApiRequestException {
-		List<MathEquation> returnList = new ArrayList<>();
-		returnList = FileUtil.loadMathEquation();
-		
-		return returnList;
-	}
+	public List<MathEquation> equationlist() throws ApiRequestException;
 }
--- src/main/java/com/mynt/parcelpricing/service/VoucherService.java
+++ src/main/java/com/mynt/parcelpricing/service/VoucherService.java
@@ -1,15 +1,8 @@
 package com.mynt.parcelpricing.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import com.mynt.parcelpricing.config.ConfigProperties;
-import com.mynt.parcelpricing.exception.ApiRequestException;
 import com.mynt.parcelpricing.model.VoucherItem;
-
-import reactor.core.publisher.Mono;
 
 /**
  * Service class that handles the voucher code retrieval and the computation.
@@ -18,13 +11,7 @@ import reactor.core.publisher.Mono;
  * @since 4/27/2021
  */
 @Service
-public class VoucherService {
-
-	@Autowired
-	private WebClient.Builder webClientBuilder;
-
-	@Autowired
-	private ConfigProperties configProps;
+public interface VoucherService {
 
 	/**
 	 * Gets the voucher details from the the mock API mynt service.
@@ -32,26 +19,5 @@ public class VoucherService {
 	 * @param voucherCode - the voucher code parameter.
 	 * @return the VoucherItem object.
 	 */
-	public VoucherItem getVoucher(String voucherCode) {
-
-		String uri = configProps.getVoucherServiceApi() + voucherCode + "?key=" + configProps.getVoucherServiceApiKey();
-		VoucherItem voucher = webClientBuilder.build()
-				.get().uri(uri)
-				.retrieve().onStatus(HttpStatus::is4xxClientError, response->{
-					Mono<String> errorMsg = response.bodyToMono(String.class);
-					return errorMsg.flatMap(msg->{
-						throw new ApiRequestException(msg);
-					});
-				})
-				.onStatus(HttpStatus::is5xxServerError, response->{
-					Mono<String> errorMsg = response.bodyToMono(String.class);
-					return errorMsg.flatMap(msg->{
-						throw new ApiRequestException(msg);
-					});
-				})
-				.bodyToMono(VoucherItem.class)
-				.block();
-
-		return voucher;
-	}
+	public VoucherItem getVoucher(String voucherCode);
 }
